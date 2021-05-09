ce48f2550941602b90d6e12062740b703905de11
Rob <rob@example.com>
2021-05-09 20:16:29 +0300

    deploy

--- src/service/service.js
+++ src/service/service.js
@@ -1,7 +1,7 @@
 class Service {
   static async getWeatherData(id) {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${id}&lang=ru&appid=a9ebdb68b0dbf40182f5638efcca3afd&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${id}&lang=ru&appid=a9ebdb68b0dbf40182f5638efcca3afd&units=metric`
     );
     try {
       return response.json();
